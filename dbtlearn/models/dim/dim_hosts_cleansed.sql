f7fdca2e951fd43ace6defa4cae6332214a99dd4
Anirban Bose <158377524+oneerbaan@users.noreply.github.com>
2025-09-17 00:35:05 +0000

    dim hosts and listings materialized as views

--- dbtlearn/models/dim/dim_hosts_cleansed.sql
+++ dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized= 'view'
+    )
+}}
+
 WITH src_hosts AS (
     SELECT*FROM {{ref('src_hosts')}}
 )
@@ -7,7 +13,7 @@ SELECT
         host_name,
         'Anonymous'
     ) AS host_name,
-    is_superhost
+    is_superhost,
     created_at,
     updated_at
 FROM
